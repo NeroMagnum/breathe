87bef6d7501d5ef103304e5136fd7755323c6789
NeroMagnum <luetkemjr@gmail.com>
2019-11-06 19:20:40 -0600

    BREATHE part 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-document.addEventListener("keydown", ev => console.log(ev.key));
+import font from "overprint/overprint/font";
+import textgrid from "overprint/overprint/text-grid"
+import cell from "overprint/overprint/cell";
+
+const canvas = document.querySelector("#game");
+
+const grid = new textgrid(canvas, {
+    width: 80,
+    height: 50,
+    font: font("menlo", false, 15)
+});
+grid.writeCell(40,25, cell("@"))
+grid.render();
+grid.writeCell(40,25, cell("@", "#f90"));
+
+const width = 40;
+const height = 30;
+
+
+const player = {
+    x: Math.floor(width / 2),
+    y: Math.floor(height / 2)
+}
+
